99db888c487cb53928a92ab7acc19b7e2c403189
DevMgit <success23236@gmail.com>
2025-03-09 12:11:09 +0530

    Create Week1.pl

---
+++ Week1.pl
@@ -0,0 +1,22 @@
+% Simple facts
+likes(ram, mango).
+girl(seema).
+likes(bill, cindy).
+red(rose).
+owns(john, gold).
+
+% Additional example facts
+person(john).
+person(mary).
+person(bob).
+
+likes(john, pizza).
+likes(mary, sushi).
+parent(john, bob).
+parent(mary, bob).
+tall(john).
+smart(mary).
+friendly(bob).
+
+age(john, 25).
+height(mary, 170).
